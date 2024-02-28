b896a8f32b0f3feff3c4ad163d76073e6611761a
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-02-28 21:40:36 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -125,8 +125,10 @@ Menampilkan source code bot yang dipakai`)
                     }
                     break;
                 case "cilik": case "alive":
-                    m.reply("SmallUbot\n    status: smallbot | founder\n    • expired: 04-August-2026\n    • server: 1\n    • dc_id: 5\n    • ping_dc: 4.792 ms\n    • cilik_uptime: 15h:24m:9s\n\n® small bot, but lots of features");
+                    const button = {buttonText: 'owner', description: 'Press the button', options: [{text: 'farid ganteng', data: 'owner'}]};
+                    await m.reply('SmallUbot\n    status: smallbot | founder\n    • expired: 04-August-2026\n    • server: 1\n    • dc_id: 5\n    • ping_dc: 4.792 ms\n    • cilik_uptime: 15h:24m:9s\n\n® small bot, but lots of features', {buttons: button});
                     break;
+
                 default: {
                     if (isCmd2 && budy.toLowerCase() != undefined) {
                         if (m.chat.endsWith("broadcast")) return;
