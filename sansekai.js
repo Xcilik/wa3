ac34f44ae637f108bbd00fcdd3f5008480406a90
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-02-29 07:26:52 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -147,10 +147,10 @@ Tanyakan apa saja kepada AI.`)
 Kirim pertanyaan kamu di bot ini, nanti dijawab sama bot ini.`			    
                     var textReply = `SmallUbot\n    status: smallbot | founder\n    • expired: 04-August-2026\n    • server: 1\n    • dc_id: 5\n    • ping_dc: 4.792 ms\n    • cilik_uptime: 15h:24m:9s\n\n® small bot, but lots of features`
                     var buttonReply = [
-				        { urlButton: { displayText: `Owner 💌`, url : `https://instagram.com/irfann._x` } },
-				        { urlButton: { displayText: `Source Code 🔗`, url: `https://github.com/rtwone/openai-botwa` } }
-			        ]
-			        tempButton(from, textReply, '', buttonReply);
+		        { urlButton: { displayText: `Owner 💌`, url : `https://instagram.com/irfann._x` } },
+			{ urlButton: { displayText: `Source Code 🔗`, url: `https://github.com/rtwone/openai-botwa` } }
+		    ]
+		    tempButton(from, textReply, '', buttonReply);
                     break;                    
                     
                 default: {
